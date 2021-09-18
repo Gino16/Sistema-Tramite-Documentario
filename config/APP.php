c65731ef51f423f6ec2e2224d27352c888ca0d46
Gino16 <gino.francisco@hotmail.com>
2021-09-17 21:48:58 -0500

    Comienzo del sistema tramite documentario

---
+++ config/APP.php
@@ -0,0 +1,5 @@
+<?php
+const SERVERURL = 'http://localhost/php/';
+const APPNOMBRE = 'Sistema de Tramite Documentario';
+
+date_default_timezone_set('America/Lima');
---
+++ config/SERVER.php
@@ -0,0 +1,7 @@
+<?php
+const SERVER = 'localhost';
+const DB = 'tramite_documentario';
+const USER = 'root';
+const PASSWORD = '';
+
+const SGBD = 'mysql:host=' . SERVER . ';dbname=' . DB;
