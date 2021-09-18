198eb5da5a5e526b1a1f0df416f78505db06587e
Gino Ascencio <gino.francisco@hotmail.com>
2021-09-18 17:03:37 -0500

    Creado branch mastar para Linux

--- config/APP.php
+++ config/APP.php
@@ -1,5 +1,5 @@
 <?php
-const SERVERURL = 'http://localhost/php/sistema-tramite-documentario/';
+const SERVERURL = 'http://localhost/php/Sistema-Tramite-Documentario//';
 const APPNAME = 'Sistema de Tramite Documentario';
 
 date_default_timezone_set('America/Lima');
