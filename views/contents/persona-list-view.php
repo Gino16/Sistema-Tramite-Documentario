121536c524c77b2167606edaa34e69ad262fad16
Gino Ascencio <gino.francisco@hotmail.com>
2021-10-17 20:00:17 -0500

    Agregando Sidebar

--- views/contents/persona-list-view.php
+++ views/contents/persona-list-view.php
@@ -5,32 +5,32 @@
     </div>
     <div class="card-body">
       <div class="input-group my-3">
-        <a class="btn btn-success" href="<?=SERVERURL?>./persona-new">Registrar Nueva Persona</a>
+        <a class="btn btn-success" href="<?= SERVERURL ?>./persona-new">Registrar Nueva Persona</a>
       </div>
-        <div class="table-responsive">
-          
-          <table class="table table-striped table-hover ">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>DNI/RUC</th>
-                <th>Apellidos y Nombre</th>
-                <th>Correo</th>
-                <th>Cod. Estudiante</th>
-<!--                <th>Puesto</th>-->
-<!--                <th>Acciones</th>-->
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              require_once './controllers/personaController.php';
-              $personaController = new PersonaController();
-              echo $personaController->paginarPersonasController($pagina[1], 1, '', $pagina[0], "");
-              ?>
+      <div class="table-responsive">
+
+        <table class="table table-striped table-hover ">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>DNI/RUC</th>
+              <th>Apellidos y Nombre</th>
+              <th>Correo</th>
+              <th>Cod. Estudiante</th>
+              <!--                <th>Puesto</th>-->
+              <!--                <th>Acciones</th>-->
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            require_once './controllers/personaController.php';
+            $personaController = new PersonaController();
+            echo $personaController->paginarPersonasController($pagina[1], 1, '', $pagina[0], "");
+            ?>
+      </div>
     </div>
   </div>
-</div>
 
-<?php
+  <?php
 
-?>
+  ?>
--- views/plantilla.php
+++ views/plantilla.php
@@ -2,29 +2,30 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<?php include './views/inc/Link.php' ?>
-    <title><?= APPNAME ?></title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php include './views/inc/Link.php' ?>
+  <title><?= APPNAME ?></title>
 </head>
 
 <body>
-<?php
-$peticionAjax = false;
-require_once './controllers/viewsController.php';
-$viewController = new ViewsController();
+  <?php
+  $peticionAjax = false;
+  require_once './controllers/viewsController.php';
+  $viewController = new ViewsController();
 
-$views = $viewController->getViewsController();
+  $views = $viewController->getViewsController();
 
-if ($views == 'login' || $views == '404') {
-	require_once './views/contents/' . $views . '-view.php';
-} else {
-	$pagina = explode("/", $_GET['views']);
-	include $views;
-}
+  if ($views == 'login' || $views == '404') {
+    require_once './views/contents/' . $views . '-view.php';
+  } else {
+    $pagina = explode("/", $_GET['views']);
+    require_once './views/inc/Sidebar.php';
+    include $views;
+  }
 
-include './views/inc/Script.php' ?>
+  include './views/inc/Script.php' ?>
 </body>
 
 </html>
